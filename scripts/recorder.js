194077c3066ddb520a8e3d1f6f0611e6128493ff
🗣🗣:"Uồầy hợp lý quãi chất thế nhờ"🔥🔥🔥🗿🗿 <148612010+901D3@users.noreply.github.com>
2025-09-23 14:23:36 +0700

    Update recorder.js | recorded video download link

--- scripts/recorder.js
+++ scripts/recorder.js
@@ -67,7 +67,7 @@ function startRecording() {
     downloadLink.download = (gId("recorderFileName").value || "video") + "." + recorderVideoFileExt;
     downloadLink.href = recordedVideoUrl;
     downloadLink.click();
-    printLog("Download link: " + downloadLink);
+    printLog("Download link: <a href='" + downloadLink + "' target='_blank'>" + downloadLink + "</a>");
   };
 
   const mimeTypeString = recorderMimeType + ";" + " codecs=" + recorderVideoCodec;
