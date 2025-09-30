ebbc5d92c30be3434f927dffa664330a41627cf9
🗣🗣:"Uồầy hợp lý quãi chất thế nhờ"🔥🔥🔥🗿🗿 <148612010+901D3@users.noreply.github.com>
2025-09-30 21:19:07 +0700

    Update generator.js | wrapper for blue nosie generating

--- scripts/generator.js
+++ scripts/generator.js
@@ -27,205 +27,63 @@ function bayerGen(size) {
 
     seed = mat;
   }
-  
+
   return seed;
 }
 
-function noiseInit(width, height, maxPoints, seed) {
-  const sqSz = width * height;
-  const indices = new Int8Array(sqSz);
+function blueNoiseWrapper() {
+  blueNoiseCanvas.width = blueNoiseWidth;
+  blueNoiseCanvas.height = blueNoiseHeight;
+  const t0 = performance.now();
+  const result = blueNoise.voidAndCluster(
+    blueNoiseWidth,
+    blueNoiseHeight,
+    Number(document.getElementById("blueNoisePDSRadiusX").value),
+    Number(document.getElementById("blueNoisePDSRadiusY").value),
+    Number(document.getElementById("blueNoisePDSKValue").value),
+    Number(document.getElementById("blueNoisePhase1Sigma").value),
+    Number(document.getElementById("blueNoisePhase2Sigma").value),
+    Number(document.getElementById("blueNoisePhase3Sigma").value),
+    Number(document.getElementById("blueNoisePhase1KernelRadius").value),
+    Number(document.getElementById("blueNoisePhase2KernelRadius").value),
+    Number(document.getElementById("blueNoisePhase3KernelRadius").value),
+    blueNoiseInitArray
+  );
+  printLog("Generating took " + (performance.now() - t0) + "ms");
+  const frame = blueNoiseCtx.getImageData(0, 0, blueNoiseWidth, blueNoiseHeight);
+  const imageData = frame.data;
+  const sqSz = blueNoiseWidth * blueNoiseHeight;
+  const sqSz4 = sqSz * 4;
+  const denom = (1 / findHighest(result)) * 255;
 
-  for (let i = 0; i < maxPoints; i++) indices[i] = 1;
+  for (let i = 0; i < sqSz4; i += 4) imageData[i + 3] = 255;
 
-  for (let i = 0; i < sqSz; i++) {
-    let j = RAND(floor(RANDD(seed ^ i) * (i + 1)));
-    [indices[i], indices[j]] = [indices[j], indices[i]];
-  }
-
-  return indices;
-}
-
-function PDSInit(width, height, sqSz, minDist, maxPoints, seed) {
-  const result = new Uint8Array(sqSz);
-  const cellSize = minDist * 0.70710678118;
-  const gridWidth = ceil(width / cellSize);
-  const gridHeight = ceil(height / cellSize);
-  const minDistSq = pow(minDist, 2);
-  const grid = new Int32Array(gridWidth * gridHeight).fill(-1);
-  const active = [];
-  const points = [];
-  let activeLength = 0;
-  let pointsLength = 0;
-  const gridIndex = (x, y) => y * gridWidth + x;
-  const insertPoint = (x, y) => {
-    const gx = floor(x / cellSize);
-    const gy = floor(y / cellSize);
-    let idx = gridIndex(gx, gy);
-    grid[idx] = points.length;
-
-    points.push({x, y});
-    active.push({x, y});
-  };
-
-  function inBounds(x, y) {
-    x >= 0 && x < width && y >= 0 && y < height;
-  }
-
-  function isFarEnough(x, y) {
-    const gx = floor(x / cellSize);
-    const gy = floor(y / cellSize);
-
-    for (let oy = -2; oy <= 2; oy++) {
-      const ny = gy + oy;
-      if (ny < 0 || ny >= gridHeight) continue;
-
-      for (let ox = -2; ox <= 2; ox++) {
-        const nx = gx + ox;
-        if (nx < 0 || nx >= gridWidth) continue;
-
-        const idx = grid[gridIndex(nx, ny)];
-        if (idx === -1) continue;
-
-        const pt = points[idx];
-        const dx = pt.x - x;
-        const dy = pt.y - y;
-        if (pow(dx, 2) + pow(dy, 2) < minDistSq) return false;
-      }
-    }
-
-    return true;
-  }
-
-  insertPoint(RAND(seed) * width, RAND(seed) * height);
-
-  activeLength = active.length;
-  pointsLength = points.length;
-  while (activeLength && pointsLength < maxPoints) {
-    const i = (RAND(seed) * activeLength) | 0;
-    const baseX = active[i].x;
-    const baseY = active[i].y;
-    const dist = minDist * (1 + RAND(seed));
-    let found = false;
-
-    for (let j = 0; j < 30; j++) {
-      const angle = RAND(seed) * 6.28318530718;
-      const x = baseX + cos(angle) * dist;
-      const y = baseY + sin(angle) * dist;
-
-      if (inBounds(x, y) && isFarEnough(x, y)) {
-        insertPoint(x, y);
-        found = true;
-        break;
-      }
-    }
-
-    if (!found) {
-      active[i] = active[activeLength - 1];
-      active.pop();
+  for (let y = 0; y < blueNoiseHeight; y++) {
+    const yOffs = y * blueNoiseWidth;
+    for (let x = 0; x < blueNoiseWidth; x++) {
+      let i = yOffs + x;
+      const v = Math.floor(result[i] * denom);
+      i <<= 2;
+      imageData[i] = v;
+      imageData[i + 1] = v;
+      imageData[i + 2] = v;
     }
   }
 
-  for (let i = 0; i < pointsLength - 1; i++) {
-    const pt = points[i];
-    result[(pt.y | 0) * width + (pt.x | 0)] = 1;
-  }
-
-  return result;
-}
-
-function blueNoise(sigmaInput, maxPoints, swLoopLimit, width, height, seed) {
-  const sqSz = width * height;
-  let nrg = new Float32Array(sqSz);
-  const widthd2 = width >> 1;
-  const heightd2 = height >> 1;
-  const maxDist2 = pow(width, 2) + pow(height, 2);
-  const expTable = new Float32Array(maxDist2);
-  const seeded = RANDDD(seed) << 10;
-  const binaryArr = gId("blueNoisePoissonDiskInit").checked
-    ? PDSInit(width, height, sqSz, pFl(gIdV("blueNoiseMinDistance")), maxPoints, seeded)
-    : noiseInit(width, height, maxPoints, seeded);
-  const actualPoints = binaryArr.reduce((sum, val) => sum + val, 0);
-
-  for (let i = 0; i < maxDist2; i++) {
-    expTable[i] = exp(i * (-1 / (sigmaInput << 1) ** 2)); //sigma
-  }
-
-  function adjNrg(idx, sign) {
-    let xa = idx % width,
-      ya = (idx / width) | 0;
-    for (let i = 0; i < sqSz; i++) {
-      let dx = (i % width) - xa,
-        dy = ((i / width) | 0) - ya;
-      if (abs(dx) > widthd2) dx += dx > 0 ? -width : width;
-      if (abs(dy) > heightd2) dy += dy > 0 ? -height : height;
-      nrg[i] += sign * expTable[dx * dx + dy * dy];
+  matrixInput = [];
+  for (let y = 0; y < blueNoiseHeight; y++) {
+    const yOffs = y * blueNoiseWidth;
+    if (!matrixInput[y]) matrixInput[y] = [];
+    for (let x = 0; x < blueNoiseWidth; x++) {
+      matrixInput[y][x] = Math.floor(result[yOffs + x] * denom);
     }
   }
 
-  maxPoints = min(maxPoints, actualPoints);
+  const highest = findHighest(matrixInput.flat()) + 1;
+  gId("matrixInput").value = formatNestedArray(matrixInput);
+  gId("divisionInput").value = highest;
+  divisionInput = highest;
+  matrixInputLUTCreate();
 
-  for (let i = 0; i < sqSz; i++) {
-    if (binaryArr[i] === 1) {
-      adjNrg(i, 1);
-    }
-  }
-
-  // swapping stage
-  while (i++ < swLoopLimit) {
-    let bestE = -Infinity,
-      bestIdx = -1,
-      worstE = Infinity,
-      worstIdx = -1;
-
-    for (let j = 0; j < sqSz; j++) {
-      if (binaryArr[j] === 1 && nrg[j] > bestE) {
-        bestE = nrg[j];
-        bestIdx = j;
-      }
-      if (binaryArr[j] === 0 && nrg[j] < worstE) {
-        worstE = nrg[j];
-        worstIdx = j;
-      }
-    }
-
-    if (worstIdx === -1 || bestIdx === worstIdx) break;
-
-    binaryArr[bestIdx] = 0;
-    adjNrg(bestIdx, -1);
-    binaryArr[worstIdx] = 1;
-    adjNrg(worstIdx, 1);
-  }
-
-  // rank assignment
-  let pR = maxPoints;
-  const rk = new Int16Array(sqSz << 2);
-  for (let i = 0; i < sqSz; i++) {
-    let m1 = -Infinity,
-      m2 = Infinity,
-      idx1 = -1,
-      idx2 = -1;
-    for (let j = 0; j < sqSz; j++) {
-      if (i < maxPoints && nrg[j] > m1 && binaryArr[i] === 1) {
-        m1 = nrg[j];
-        idx1 = j;
-      }
-      if (nrg[j] < m2 && binaryArr[j] === 0) {
-        m2 = nrg[j];
-        idx2 = j;
-      }
-    }
-    if (i < maxPoints && binaryArr[i] === 1) {
-      if (idx1 === -1) break;
-      binaryArr[idx1] = 0;
-      adjNrg(idx1, -1);
-      rk[idx1] = --pR;
-    } else {
-      if (idx2 === -1) break;
-      binaryArr[idx2] = 1;
-      adjNrg(idx2, 1);
-      rk[idx2] = i;
-    }
-  }
-
-  return rk;
+  blueNoiseCtx.putImageData(frame, 0, 0);
 }
