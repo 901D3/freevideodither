0f1f3d16bfc250650f2a6a84254cc337ab5310d5
🗣🗣: "ùồầỳ hợp lý quãi chất thế nhờ"🤫🗿🔥🔥 <148612010+901D3@users.noreply.github.com>
2025-12-01 16:57:59 +0700

    Update blue-noise-float64.js

--- scripts/lib/901D3/blue-noise-float64.js
+++ scripts/lib/901D3/blue-noise-float64.js
@@ -2,8 +2,8 @@
 
 (function () {
   const scriptElement = document.createElement("script");
-  scriptElement.type;
   scriptElement.src = "https://901d3.github.io/blue-noise.js/src/blue-noise-float64.js";
 
   document.head.appendChild(scriptElement);
 })();
+
