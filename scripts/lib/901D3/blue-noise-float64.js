af5f5fedf5c4c22491f9c78aac1139bfb0a9dfbb
🗣🗣: "ùồầỳ hợp lý quãi chất thế nhờ"🤫🗿🔥🔥 <148612010+901D3@users.noreply.github.com>
2025-12-01 16:57:48 +0700

    Update and rename blue-noise-float32.js to blue-noise-float64.js

---
+++ scripts/lib/901D3/blue-noise-float64.js
@@ -0,0 +1,9 @@
+// Reduce copying scripts
+
+(function () {
+  const scriptElement = document.createElement("script");
+  scriptElement.type;
+  scriptElement.src = "https://901d3.github.io/blue-noise.js/src/blue-noise-float64.js";
+
+  document.head.appendChild(scriptElement);
+})();
