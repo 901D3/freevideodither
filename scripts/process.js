22bd03c664fd05375d74fda28b849f6f954bc409
🗣🗣: "ùồầỳ hợp lý quãi chất thế nhờ"🤫🗿🔥🔥 <148612010+901D3@users.noreply.github.com>
2025-12-02 17:00:03 +0700

    Update process.js

--- scripts/process.js
+++ scripts/process.js
@@ -331,9 +331,92 @@ function processFrame() {
   ctx.drawImage(video, 0, 0, canvasWidth, canvasHeight);
   const frame = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
   const imageData = frame.data;
+
   if (useLinear) {
-    for (let i = 0, length = imageData.length; i < length; i++) {
-      imageData[i] = floor(linearLUT[imageData[i]]);
+    for (let i = imageData.length; i >= 0; i--) {
+      imageData[i] = linearLUT[imageData[i]] | 0;
+    }
+  }
+
+  let sqSz;
+  let rChannel;
+  let gChannel;
+  let bChannel;
+
+  if (useDBS) {
+    sqSz = canvasWidth * canvasHeight;
+
+    rChannel = new Uint8Array(sqSz);
+    gChannel = new Uint8Array(sqSz);
+    bChannel = new Uint8Array(sqSz);
+
+    for (let i = 0; i < sqSz; i++) {
+      const channelIdx = i * 4;
+
+      rChannel[i] = imageData[channelIdx];
+      gChannel[i] = imageData[channelIdx + 1];
+      bChannel[i] = imageData[channelIdx + 2];
+    }
+  }
+
+  d[ditherDropdownValue](imageData);
+
+  let rChannelDithered;
+  let gChannelDithered;
+  let bChannelDithered;
+
+  if (useDBS) {
+    rChannelDithered = new Uint8Array(sqSz);
+    gChannelDithered = new Uint8Array(sqSz);
+    bChannelDithered = new Uint8Array(sqSz);
+
+    for (let i = 0; i < sqSz; i++) {
+      const channelIdx = i * 4;
+
+      rChannelDithered[i] = imageData[channelIdx] > 127 ? 1 : 0;
+      gChannelDithered[i] = imageData[channelIdx + 1] > 127 ? 1 : 0;
+      bChannelDithered[i] = imageData[channelIdx + 2] > 127 ? 1 : 0;
+    }
+  }
+
+  if (useDBS) {
+    blueNoiseFloat64.directBinarySearch(
+      rChannel,
+      rChannelDithered,
+      canvasWidth,
+      canvasHeight,
+      DBSSigma,
+      DBSIterations,
+      blueNoiseCustomKernel
+    );
+
+    blueNoiseFloat64.directBinarySearch(
+      gChannel,
+      gChannelDithered,
+      canvasWidth,
+      canvasHeight,
+      DBSSigma,
+      DBSIterations,
+      blueNoiseCustomKernel
+    );
+
+    blueNoiseFloat64.directBinarySearch(
+      bChannel,
+      bChannelDithered,
+      canvasWidth,
+      canvasHeight,
+      DBSSigma,
+      DBSIterations,
+      blueNoiseCustomKernel
+    );
+
+    for (let i = 0; i < sqSz; i++) {
+      const channelIdx = i * 4;
+
+      imageData[channelIdx] = (rChannelDithered[i] * 255) | 0;
+      imageData[channelIdx + 1] = (gChannelDithered[i] * 255) | 0;
+      imageData[channelIdx + 2] = (bChannelDithered[i] * 255) | 0;
+      imageData[channelIdx + 3] = 255;
     }
   }
 
