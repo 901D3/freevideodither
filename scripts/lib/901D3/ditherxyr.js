bc1678d09606d451a74ce71498438310d9dfd620
Xyrherris <148612010+901D3@users.noreply.github.com>
2026-06-20 03:54:23 +0700

    Create ditherxyr.js

---
+++ scripts/lib/901D3/ditherxyr.js
@@ -0,0 +1,6 @@
+(function () {
+  const scriptElement = document.createElement("script");
+  scriptElement.src = "https://901d3.github.io/ditherxyr.js/ditherxyr.js";
+
+  document.head.appendChild(scriptElement);
+})();
