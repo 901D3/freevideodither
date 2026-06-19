df812ea1a884d8fe3009b460c3c2d05d513ed06a
Xyrherris <148612010+901D3@users.noreply.github.com>
2026-06-20 03:51:50 +0700

    Removed comment

--- scripts/lib/901D3/WebM-muxer.js
+++ scripts/lib/901D3/WebM-muxer.js
@@ -1,5 +1,3 @@
-// Reduce copying scripts
-
 (function () {
   const scriptElement = document.createElement("script");
   scriptElement.src = "https://901d3.github.io/WebM-muxer.js/src/WebM-muxer.js";
