0971146db140cff9d382946c75700e688d7f36e1
Xyrherris <148612010+901D3@users.noreply.github.com>
2026-06-20 03:49:51 +0700

    Delete scripts/lib/901D3/blue-noise-utils.js

--- scripts/lib/901D3/blue-noise-utils.js
+++
@@ -1,8 +0,0 @@
-// Reduce copying scripts
-
-(function () {
-  const scriptElement = document.createElement("script");
-  scriptElement.src = "https://901d3.github.io/blue-noise.js/src/blue-noise-utils.js";
-
-  document.head.appendChild(scriptElement);
-})();
