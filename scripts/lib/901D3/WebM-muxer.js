55da220e909bca6c95b379c6b414d2143b101e09
Xyrherris <148612010+901D3@users.noreply.github.com>
2026-06-20 04:02:28 +0700

    Delete scripts/lib/901D3 directory

--- scripts/lib/901D3/WebM-muxer.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  const scriptElement = document.createElement("script");
-  scriptElement.src = "https://901d3.github.io/WebM-muxer.js/src/WebM-muxer.js";
-
-  document.head.appendChild(scriptElement);
-})();
--- scripts/lib/901D3/ditherxyr.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  const scriptElement = document.createElement("script");
-  scriptElement.src = "https://901d3.github.io/ditherxyr.js/ditherxyr.js";
-
-  document.head.appendChild(scriptElement);
-})();
